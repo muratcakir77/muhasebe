d2bf60afc564b4a538ed3d93b80f70875058e9ea
SARAY CAFE <SARAY CAFE@MASA-28>
2024-06-06 15:29:31 +0300

    Proje dosyası ekle.

---
+++ muhasebe/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace muhasebe_uygulaması._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(737, 509);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOlusturListele;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSonPersonel;
+        private System.Windows.Forms.Button btn400Arttır;
+        private System.Windows.Forms.Button btnToplamMaas;
+        private System.Windows.Forms.Button bnt4Bin8Bin;
+        private System.Windows.Forms.Button btn4BindenAz;
+        private System.Windows.Forms.Button btnYuksekMaasPers;
+        private System.Windows.Forms.Button btnDusukMaas;
+        private System.Windows.Forms.Button btnToplamPersonel;
+        private System.Windows.Forms.Button btnListeTemizle;
+        private System.Windows.Forms.ListBox lbgoster;
+        private System.Windows.Forms.TextBox tbPersonelAra;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ muhasebe/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace muhasebe_uygulaması._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] personeller;
+        int[] maaslar;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string aranan = tbPersonelAra.Text;
+            bool bulundu = false;
+
+            for (int i = 0; i < personeller.Length; i++)
+            {
+
+                if (aranan == personeller[i])
+                {
+                    bulundu = true;
+                    lbGoster.SelectedIndex = i;
+                    break;
+                }
+
+
+            }
+
+            if (bulundu)
+            {
+                MessageBox.Show("Personel Bulundu.");
+            }
+            else
+            {
+                MessageBox.Show("Personel Bulunamadı.");
+            }
+
+            tbPersonelAra.Clear();
+
+        }
+
+      
+
+        private void btnToplamMaas_Click(object sender, EventArgs e)
+        {
+            int toplamMaas = 0;
+            for (int i = 0; i < maaslar.Length; i++)
+            {
+                /*toplamMaas += maaslar[i];*/
+                toplamMaas = toplamMaas + maaslar[i];
+            }
+            lblSonuc.Text = "Listedeki Toplam Maaş : " + toplamMaas.ToString();
+
+        }
+
+        private void btnOlusturListele_Click(object sender, EventArgs e)
+        {
+            lbGoster.Items.Clear();
+            personeller = new string[] { "Murat", "Aydanur", "Sevil", "Yelda", "Ahmet" };
+            maaslar = new int[] { 6000, 7000, 4200, 7900, 9500 };
+
+            for (int i = 0; i < personeller.Length; i++)
+            {
+
+                lbGoster.Items.Add(personeller[i] + "\t" + maaslar[i]);
+
+            }
+
+        }
+
+
+        private void btnListeTemizle_Click(object sender, EventArgs e)
+        {
+            lbGoster.Items.Clear();
+        }
+
+        private void btnToplamPersonel_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+
+            for (int i = 0; i < personeller.Length; i++)
+            {
+                sayac = sayac + 1;
+            }
+            lblSonuc.Text = "Sonuç : " + sayac.ToString();
+
+            lblSonuc.Text = "Toplam Personel Sayısı : " + personeller.Length;
+
+        }
+
+        private void btnDusukMaas_Click(object sender, EventArgs e)
+        {
+            int min;
+            min = maaslar[0];
+            for (int i = 0; i < maaslar.Length; i++)
+            {
+                if (maaslar[i] < min)
+                {
+                    min = maaslar[i];
+                }
+                lblSonuc.Text = "En Düşük Maaş : " + min.ToString();
+            }
+
+        }
+
+        private void btnYuksekMaasPers_Click(object sender, EventArgs e)
+        {
+            /*int max;
+string maxpersonel="";
+max = maaslar[0];
+for(int i = 0; i < maaslar.Length; i++)
+{
+    if (maaslar[i] > max)
+    {
+        max = maaslar[i];
+        maxpersonel = personeller[i];
+    }
+
+
+}
+lblSonuc.Text = "En Yüksek Maaşı Alan Personel : " + maxpersonel;*/
+
+
+            int max;
+            int maxIndex = 0;
+            max = maaslar[0];
+            for (int i = 0; i < maaslar.Length; i++)
+            {
+                if (maaslar[i] > max)
+                {
+                    max = maaslar[i];
+                    maxIndex = i;
+                }
+
+
+            }
+            lblSonuc.Text = "En Yüksek Maaşı Alan Personel : " + personeller[maxIndex];
+
+        }
+
+        private void btn4BindenAz_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < maaslar.Length; i++)
+            {
+                if (maaslar[i] < 4000)
+                {
+                    lbGoster.Items.Add(personeller[i] + ">>" + maaslar[i]);
+                }
+            }
+
+
+        }
+
+        private void bnt4Bin8Bin_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+
+            for (int i = 0; i < maaslar.Length; i++)
+            {
+                if ((maaslar[i] >= 4000) && (maaslar[i] <= 8000))
+                {
+                    sayac = sayac + 1;
+                }
+                lblSonuc.Text = "Maaşı 4000 - 8000 Arasında Olan Personel Sayısı : " + sayac.ToString();
+            }
+
+        }
+
+        private void btn400Arttır_Click(object sender, EventArgs e)
+        {
+            lbGoster.Items.Clear();
+            maaslar[2] += 400;
+
+            for (int i = 0; i < personeller.Length; i++)
+            {
+                lbGoster.Items.Add(personeller[i] + "\t" + maaslar[i]);
+            }
+
+
+        }
+
+        private void btnSonPersonel_Click(object sender, EventArgs e)
+        {
+            lblSonuc.Text = "Listedeki Son Personelin Adı : " + personeller[4];
+        }
+    }
+}
